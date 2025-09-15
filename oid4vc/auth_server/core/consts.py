a7c5df9438008dc807145ac251309ef96bf515f3
Ivan Wei <ivan.wei@ontario.ca>
2025-09-15 14:43:46 -0400

    Add client auth (#26)
    * enhance: improve Azure Files sync reliability and use correct compression libs for status list types
    
    Signed-off-by: Ivan Wei <ivan.wei@ontario.ca>
    
    * feat: enable standalone auth server for oid4vci
    
    Signed-off-by: Ivan Wei <ivan.wei@ontario.ca>
    
    * add /nonce route, rename m2m to internal
    
    Signed-off-by: Ivan Wei <ivan.wei@ontario.ca>
    
    * remove extra line spaces
    
    Signed-off-by: Ivan Wei <ivan.wei@ontario.ca>
    
    * auth tenant subpath
    
    * add auth client
    
    * re-org auth client code
    
    ---------
    
    Signed-off-by: Ivan Wei <ivan.wei@ontario.ca>

---
+++ oid4vc/auth_server/core/consts.py
@@ -0,0 +1,30 @@
+"""Global constants."""
+
+
+class OAuth2Flow:
+    """OAuth2 grant types."""
+
+    PRE_AUTH_CODE = "pre_auth_code"
+    REFRESH_TOKEN = "refresh_token"
+
+
+class OAuth2GrantType:
+    """OAuth2 grant types."""
+
+    PRE_AUTH_CODE = "urn:ietf:params:oauth:grant-type:pre-authorized_code"
+    REFRESH_TOKEN = "refresh_token"
+
+
+class ClientAuthMethod:
+    """OAuth2 client authentication methods."""
+
+    PRIVATE_KEY_JWT = "private_key_jwt"
+    CLIENT_SECRET_BASIC = "client_secret_basic"
+    SHARED_BEARER = "shared_bearer"
+
+
+CLIENT_AUTH_METHODS: tuple[str, ...] = (
+    ClientAuthMethod.PRIVATE_KEY_JWT,
+    ClientAuthMethod.CLIENT_SECRET_BASIC,
+    ClientAuthMethod.SHARED_BEARER,
+)
